b466d58cb49d26e7d191ae739c26cd39a6129da7
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2024-09-18 22:03:49 +0200

    Actualiza nombre del archivo .java, README y los comentarios

--- AhorcadoII/README.md
+++ AhorcadoII/README.md
@@ -1,18 +1,24 @@
-## Getting Started
+# Juego del Ahorcado en JAVA
+He realizado el juego donde se valida la palabra secreta y permite al jugador ver las letras incorrectas que ha ingresado y la cantidad de intentos que le quedan.
+Los intento son fijos y se establecieron en 6.
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
-
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+## Qué herramientas se utilizaron?
+1. La Clase Scanner
+2. Colores disponibles en ANSI para cambiar el color de las letras en la consola
+	*			String  ANSI_RESET  =  "\u001B[0m";
+				String  ANSI_RED  =  "\u001B[31m";
+				String  ANSI_BLUE  =  "\u001B[34m";
+				String  ANSI_GREEN  =  "\u001B[32m";
+				String  ANSI_YELLOW  =  "\u001B[33m";
+3. Se usan los métodos de la clase Scanner para capturar un String y convertirlo en minúscula. 
+	*			palabraSecreta  =  scanner.nextLine().toLowerCase();
+4. Se usan los métodos de la clase Character para convertir en minúscula y de la clase Scanner next() que provoca que el programa “espere” a que el usuario escriba algo por teclado y charAt(0) filtra y captura solo el primer caracter introducido.
+	*			char  correcta  =  Character.toLowerCase(scanner.next().charAt(0));
+5. Verificamos si hay alguna entrada restante en el búfer con el método scanner.hasNextLine() y lo borramos llamando a scanner.nextLine()
+	*			if (scanner.hasNextLine()) {scanner.nextLine();}
+6. Se usa en el for un char casting y se coloca instrucción especial para rellenar el arreglo con los números del 6 5 4 3 2 1 para luego ser suplantados por las letras incorrectas
+	*			for (int  i  =  0; i  <  letraIncorrecta.length; i++) {letraIncorrecta[i] = (char) ('0'  +  intentosMaximos  -  i);}
+7. Se hace uso del método valueOf de la clase String para tratar el arreglo de tipo char como si de un String se tratara al imprimirlo en pantalla
+	*			println("\tAdivina la palabra secreta: "  +  String.valueOf(letraAdivinar));
+8. Se utiliza un comando para limpiar la pantalla de la consola y hacer más amigable la interfaz.
+	*			System.out.print("\033[H\033[2J"); // Limpia la pantalla
--- AhorcadoII/src/AhorcadoII.java
+++ AhorcadoII/src/AhorcadoII.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
-public class App {
+public class AhorcadoII {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+
+        // Colores disponibles en ANSI para cambiar el color de las letras en la consola
         String ANSI_RESET = "\u001B[0m";
         String ANSI_RED = "\u001B[31m";
         String ANSI_BLUE = "\u001B[34m";
@@ -16,6 +18,7 @@ public class App {
         int intentos = 0;
         char[] letraIncorrecta = new char[intentosMaximos];
 
+        // Estructura de control iterativa para validar la palabra secreta
         while (!palabraCorrecta) {
             System.out.print("\033[H\033[2J"); // Limpia la pantalla
             System.out.println(ANSI_BLUE + "**** BIENVENIDO AL JUEGO DEL AHORCADO ****\nEs el turno del " + ANSI_RESET
@@ -23,11 +26,18 @@ public class App {
             System.out.println(ANSI_YELLOW + "Debe elegir la palabra secreta.");
             System.out.print("Escríbala: " + ANSI_RESET);
 
+            // Se usan los métodos de la clase Scanner para capturar un String y convertirlo
+            // en minúscula.
             palabraSecreta = scanner.nextLine().toLowerCase();
             System.out.print("Está de acuerdo que esta sea la palabra secreta (Y/N): ");
+
+            // Se usan los métodos de la clase Character para convertir en minúscula y de la
+            // clase Scanner next() que provoca que el programa “espere” a que el usuario
+            // escriba algo por teclado y charAt(0) filtra y captura solo el primer caracter
+            // introducido.
             char correcta = Character.toLowerCase(scanner.next().charAt(0));
 
-            // verificamos si hay alguna entrada restante en el búfer con el método
+            // Verificamos si hay alguna entrada restante en el búfer con el método
             // scanner.hasNextLine() y lo borramos llamando a scanner.nextLine()
             if (scanner.hasNextLine()) {
                 scanner.nextLine();
@@ -44,6 +54,7 @@ public class App {
             letraAdivinar[i] = '_';
         }
 
+        //Se usa en el for un char casting y se coloca instrucción especial para rellenar el arreglo con los números del 6 al 1
         for (int i = 0; i < letraIncorrecta.length; i++) {
             letraIncorrecta[i] = (char) ('0' + intentosMaximos - i);
         }
@@ -61,7 +72,7 @@ public class App {
             System.out.print(ANSI_YELLOW + "\nTienes " + (intentosMaximos - intentos) + " Intentos.");
             System.out.print(" PULSA UNA LETRA: " + ANSI_RESET);
 
-            char letra = scanner.next().charAt(0);
+            char letra = Character.toLowerCase(scanner.next().charAt(0));
 
             for (int i = 0; i < palabraSecreta.length(); i++) {
                 if (palabraSecreta.charAt(i) == letra) {
@@ -81,15 +92,17 @@ public class App {
                 palabraAdivinada = true;
                 System.out.print("\033[H\033[2J"); // Limpia la pantalla
                 System.out.println(ANSI_GREEN +
-                        "ENHORA BUENA!! Haz logrado adivinar la palabra secreta.\n\n\t\t" + ANSI_RESET + ANSI_BLUE + palabraSecreta.toUpperCase()
-                                + "\n" + ANSI_RESET);
+                        "ENHORA BUENA!! Haz logrado adivinar la palabra secreta.\n\n\t\t" + ANSI_RESET + ANSI_BLUE
+                        + palabraSecreta.toUpperCase()
+                        + "\n" + ANSI_RESET);
             }
         }
 
         if (!palabraAdivinada) {
             System.out.print("\033[H\033[2J"); // Limpia la pantalla
-            System.out.println(ANSI_YELLOW + "GAME OVER!! No lograste descubrir la palabra secreta.\n\n\t\t" + ANSI_RESET
-                    + ANSI_RED + palabraSecreta.toUpperCase() + "\n" + ANSI_RESET);
+            System.out
+                    .println(ANSI_YELLOW + "GAME OVER!! No lograste descubrir la palabra secreta.\n\n\t\t" + ANSI_RESET
+                            + ANSI_RED + palabraSecreta.toUpperCase() + "\n" + ANSI_RESET);
         }
 
         scanner.close();
